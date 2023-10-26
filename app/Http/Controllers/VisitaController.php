72d5de736077a08fe231dfb071dc2f7f89a3e2f3
Marcell <marcell.nicson@bee.com.br>
2023-10-25 22:48:32 -0300

    Disparando email ao adicionar uma visita

--- app/Http/Controllers/VisitaController.php
+++ app/Http/Controllers/VisitaController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\HelloMail;
 use App\Models\Cliente;
 use App\Models\Corretor;
 use App\Models\Imovel;
 use Illuminate\Http\Request;
 use App\Models\Visita;
+use Illuminate\Support\Facades\Mail;
 
 class VisitaController extends Controller
 {
@@ -39,7 +41,34 @@ class VisitaController extends Controller
             'status' => 'required',
         ]);
 
-        Visita::create($request->all());
+        $visita = Visita::create($request->all());
+
+
+        $emailcorretor = $visita->corretor->email ? $visita->corretor->email : null;
+        $emailcliente = $visita->cliente->email ? $visita->cliente->email : null;
+
+        $dataVisita = \Carbon\Carbon::parse($visita->data_visita)->format('d/m/Y H:i:s');
+
+        if ($visita and $emailcorretor != null) {           
+
+            $titulo = 'Olá, '. $visita->corretor->nome .' há nova visita para você!';
+            $mensagem = 'Sua visita está agendada para: ' . $dataVisita . 
+            ' com o cliente: ' . $visita->cliente->nome . 
+            ' no endereço: ' . $visita->imovel->endereco;
+        
+            Mail::to($emailcorretor)->send(new HelloMail($titulo, $mensagem));                   
+
+        }
+        if($visita and $emailcliente != null){
+
+            $titulo = 'Olá, '. $visita->cliente->nome .' há nova visita para você!';
+            $mensagem = 'Sua visita está agendada para: ' . $dataVisita . 
+            ' com o Corretor ' . $visita->corretor->nome . 
+            ' no endereço: ' . $visita->imovel->endereco;
+
+            Mail::to($emailcliente)->send(new HelloMail($titulo, $mensagem));
+        
+        }
 
         return redirect()->route('visita')
             ->with('success', 'Visita criada com sucesso.');
@@ -51,12 +80,14 @@ class VisitaController extends Controller
         $clientes = Cliente::all();
         $imoveis = Imovel::all();
 
+
+
         return view('visita.edit', compact('visita', 'corretores', 'clientes', 'imoveis'));
     }
 
     public function update(Request $request)
     {   
-        $visita = Visita::find($request->id);
+        $visita = Visita::find($request->id);   
 
         $request->validate([
             'corretor_id' => 'required',
---
+++ resources/views/mail/hello.blade.php
@@ -0,0 +1,13 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>{{ $titulo }}</title>
+</head>
+<body>
+    <h1>{{ $titulo }}</h1>
+    <p>{{ $mensagem }}</p>
+</body>
+</html>
+
+
